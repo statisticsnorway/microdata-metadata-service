d3a9c6b575ec907ecf67b859d4cf1b3b861d54ad
vak <vak@macbookpro.local>
2021-03-19 14:41:47 +0100

    Uses bucket metadata repository now

--- src/main/java/no/microdata/datastore/services/DataStoreServiceImpl.java
+++ src/main/java/no/microdata/datastore/services/DataStoreServiceImpl.java
@@ -1,35 +1,25 @@
 package no.microdata.datastore.services;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import no.microdata.datastore.DataStoreService;
-import org.springframework.beans.factory.annotation.Value;
+import no.microdata.datastore.repository.MetadataRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.Map;
 
 @Service
 public class DataStoreServiceImpl implements DataStoreService {
 
-    @Value("${datastore.metadata.data-store}")
-    private String datastoreFile;
-
-    @Value("${datastore.metadata.versions}")
-    private String versionsFile;
+    @Autowired
+    @Qualifier("bucketMetadataRepository")
+    MetadataRepository metadataRepository;
 
     @Override
     public Map<String, Object> findAllDataStoreVersions(String requestId) {
-        try {
-            Map<String, Object> datastore = new ObjectMapper().readValue(
-                                Files.readString(Paths.get(datastoreFile)), Map.class);
-            Map<String, Object> versions = new ObjectMapper().readValue(
-                                Files.readString(Paths.get(versionsFile)), Map.class);
-            datastore.putAll(versions);
-            return datastore;
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        Map<String, Object> datastore = metadataRepository.getDataStoreFile();
+        Map<String, Object> versions = metadataRepository.getVersionsFile();
+        datastore.putAll(versions);
+        return datastore;
     }
 }
